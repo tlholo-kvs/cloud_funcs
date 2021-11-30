b87d498a244f7226eae91e95b3aac2006177fb82
tlholo-kvs <tlholokgoele@hotmail.com>
2021-11-30 14:37:09 +0200

    Added sendNotificationToFCMToken method

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
-exports.sendNotificationToTopic = functions.firestore.document("donate items/{uid}").onCreate(async (event) => {
+exports.sendNotificationToTopic = functions.firestore.document("reviewed donated items/{uid}").onCreate(async (event) => {
   const title = event.after.get("title");
   const description = event.after.get("description");
   // preparing the actual  notification
@@ -18,9 +18,10 @@ exports.sendNotificationToTopic = functions.firestore.document("donate items/{ui
   };
 
   await admin.messaging().send(message);
+  // console.log(respone);
 });
 
-exports.sendNotificationToTopic = functions.firestore.document("reviewed requests/{uid}").onCreate(async (event) => {
+exports.sendNotificationToTopic = functions.firestore.document("reviewed requests/{uid}").onWrite(async (event) => {
   const title = event.after.get("title");
   const description = event.after.get("description");
   // preparing the actual  notification
@@ -36,4 +37,30 @@ exports.sendNotificationToTopic = functions.firestore.document("reviewed request
   };
 
   await admin.messaging().send(message);
+  // console.log(respone);
 });
+
+
+// attempting to send notifications by using the FCM token
+// This method attempts to send notification on changes to the reviewed requests document,
+// whihc will store donations that have been fully processed and accepted
+exports.sendNotificationToFCMToken = functions.firestore.document("reviewed requests/{uid}").onWrite(async (event) =>{
+  const uid = event.after.get("userUid");
+  const userDoc = await admin.firestore().doc(`userId/${uid}`).get();
+  const title = event.after.get("title");
+  const description = event.after.get("description");
+  const fcmToken = userDoc.get("fcm");
+
+  const message = {
+    notification: {
+      title: title,
+      body: description,
+    },
+    token: fcmToken,
+  };
+
+  const response = await admin.messaging().send(message);
+  console.log(response);
+});
+
+
